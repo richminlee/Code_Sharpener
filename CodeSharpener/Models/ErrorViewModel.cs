7d83ef2e77cbda83d130c8530f51b93f753534c5
Richard Lee <lee.richardm@gmail.com>
2020-08-27 22:32:18 -0700

    began building homepage

---
+++ CodeSharpener/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeSharpener.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
