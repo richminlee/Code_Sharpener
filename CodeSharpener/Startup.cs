c9f538b0b8cef9cb86541ebb70fc6fe1ed278fea
Richard Lee <lee.richardm@gmail.com>
2020-09-30 23:28:18 -0700

    Set up Application for deployment on Azure

--- CodeSharpener/Startup.cs
+++ CodeSharpener/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -9,6 +5,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using CodeSharpener.Models;
+using Microsoft.Data.SqlClient;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+using System;
 
 namespace CodeSharpener
 {
@@ -20,15 +21,31 @@ namespace CodeSharpener
         }
 
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
+            
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+            };
+            var client = new SecretClient(new Uri("https://codesharpener2kv.vault.azure.net/"), new DefaultAzureCredential(), options);
 
+            KeyVaultSecret secret = client.GetSecret("DbPassword");
+
+            string secretValue = secret.Value;
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(Configuration.GetConnectionString("CodeConnection"));
+            builder.Password = secretValue;
             services.AddDbContext<CodeSharpenerContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CodeContext")));
+                    options.UseSqlServer(builder.ConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
