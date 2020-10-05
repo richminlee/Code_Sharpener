d3e0e3c2274df5682b5ee82266dc29b34945ea46
Richard Lee <lee.richardm@gmail.com>
2020-10-05 11:18:32 -0700

    Added Bootstrap Template

---
+++ CodeSharpener/Controllers/ChallengesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CodeSharpener.Models;
+
+namespace CodeSharpener.Controllers
+{
+    public class ChallengesController : Controller
+    {
+        private readonly CodeSharpenerContext _context;
+
+        public ChallengesController(CodeSharpenerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Challenges
+        public async Task<IActionResult> Index()
+        {
+            var codeSharpenerContext = _context.Challenge.Include(c => c.Content);
+            return View(await codeSharpenerContext.ToListAsync());
+        }
+
+        // GET: Challenges/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var challenge = await _context.Challenge
+                .Include(c => c.Content)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(challenge);
+        }
+
+        // GET: Challenges/Create
+        public IActionResult Create()
+        {
+            ViewData["ContentId"] = new SelectList(_context.Content, "Id", "Id");
+            return View();
+        }
+
+        // POST: Challenges/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,ContentId")] Challenge challenge)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(challenge);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContentId"] = new SelectList(_context.Content, "Id", "Id", challenge.ContentId);
+            return View(challenge);
+        }
+
+        // GET: Challenges/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var challenge = await _context.Challenge.FindAsync(id);
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContentId"] = new SelectList(_context.Content, "Id", "Id", challenge.ContentId);
+            return View(challenge);
+        }
+
+        // POST: Challenges/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,ContentId")] Challenge challenge)
+        {
+            if (id != challenge.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(challenge);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChallengeExists(challenge.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContentId"] = new SelectList(_context.Content, "Id", "Id", challenge.ContentId);
+            return View(challenge);
+        }
+
+        // GET: Challenges/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var challenge = await _context.Challenge
+                .Include(c => c.Content)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(challenge);
+        }
+
+        // POST: Challenges/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var challenge = await _context.Challenge.FindAsync(id);
+            _context.Challenge.Remove(challenge);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChallengeExists(int id)
+        {
+            return _context.Challenge.Any(e => e.Id == id);
+        }
+    }
+}
