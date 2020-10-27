14f68984faf6f229c67bed221dafd03217868c02
Richard Lee <lee.richardm@gmail.com>
2020-10-27 11:55:33 -0700

    added start of code editor

---
+++ CodeSharpener/wwwroot/js/default.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    var code = $(".codemirror-textarea")[0];
+    var editor = new CodeMirror.fromTextArea(code, {
+        theme: "dracula",
+        lineNumbers: true,
+        mode: "javascript"
+    })
+});
